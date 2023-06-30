d5953a9cb2943fb31faf67fecc25c7f8904aaea9
Yakov Lipkovich <no@no>
2023-06-30 20:49:47 +0000

    Make post-deploy more resilient

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ use SimpleWebApps\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+/** @psalm-suppress all */
 return function (array $context) {
   return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
--- public/postdeploy.php
+++ public/postdeploy.php
@@ -11,37 +11,63 @@ use Symfony\Component\HttpFoundation\Response;
 
 header('Content-Type: text/plain');
 
-$root = dirname(__DIR__).'/standby';
+$standby = dirname(__DIR__).'/standby';
 $online = dirname(__DIR__).'/online';
-
 $zipPath = dirname(__DIR__).'/simplewebapps.zip';
-if (file_exists($zipPath)) {
-  // Remove all existing files
-  array_map('unlink', glob($root.'/*'));
 
-  // Extract the uploaded archive
+const TIMEOUT_NS = 55_000_000_000;
+const PARAM_CONT = 'cont';
+
+$timer = hrtime(true);
+
+if (file_exists($zipPath)) {
+  // Remove all existing files if continue parameter is unset
+  if (isset($_GET[PARAM_CONT])) {
+    /** @psalm-suppress RiskyCast */
+    $fn = (int) $_GET[PARAM_CONT];
+  } else {
+    /** @psalm-suppress UnusedFunctionCall */ array_map('unlink', glob($standby.'/*'));
+    $fn = 0;
+  }
+
+  // Extract the uploaded archive, file-by-file
   $zip = new ZipArchive();
   $res = $zip->open($zipPath);
   if (true !== $res) {
     http_response_code(500);
-    exit('Failed to open zip: '.$res);
+    exit('Failed to open zip: '.(string) $res);
   }
-  $res = $zip->extractTo($root);
-  if (true !== $res) {
-    http_response_code(500);
-    exit('Failed to extract zip');
+
+  for (; $fn < $zip->count(); ++$fn) {
+    // If we're past the timeout, redirect to keep alive
+    if (hrtime(true) - $timer >= TIMEOUT_NS) {
+      /** @psalm-suppress PossiblyUndefinedArrayOffset */
+      $uri = sprintf('%s?%s=%d', trim(strtok($_SERVER['REQUEST_URI'], '?')), PARAM_CONT, $fn);
+      header("Location: $uri");
+      exit('Unzipping paused before file '.(string) $fn);
+    }
+
+    $name = $zip->getNameIndex($fn);
+    $res = $zip->extractTo($standby, $name);
+    if (true !== $res) {
+      http_response_code(500);
+      exit('Failed to extract zip');
+    }
   }
-  $zip->close();
-  unlink($zipPath);
 
   // Server might time out, so redirect to keep it alive
-  header("Location: {$_SERVER['REQUEST_URI']}");
+  $zip->close();
+  unlink($zipPath);
+  /** @psalm-suppress PossiblyUndefinedArrayOffset */
+  $uri = trim(strtok($_SERVER['REQUEST_URI'], '?'));
+  header("Location: $uri");
   exit('Finished unzipping');
 }
 
-require_once $root.'/vendor/autoload_runtime.php';
+/** @psalm-suppress MissingFile */
+require_once $standby.'/vendor/autoload_runtime.php';
 
-return function () use ($root, $online) {
+return function () use ($standby, $online) {
   $kernel = new Kernel('prod', true);
   $application = new Application($kernel);
   $application->setAutoExit(false);
@@ -70,11 +96,11 @@ return function () use ($root, $online) {
 
   try {
     $realOnline = $fs->readlink($online);
-    $realStandby = $fs->readlink($root);
+    $realStandby = $fs->readlink($standby);
     assert($realOnline && $realStandby);
 
     $fs->symlink($realStandby, $online);
-    $fs->symlink($realOnline, $root);
+    $fs->symlink($realOnline, $standby);
     $fs->remove('postdeploy.php');
   } catch (Exception $e) {
     $message = var_export($e, true);
